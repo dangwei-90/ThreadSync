57ca77d9e344b249d7591da0c2fc8ca021dcf2cf
dangwei <dangwei@360.cn>
2020-11-18 11:59:11 +0800

    [thread] add not sync demo.

--- thread_sync/thread_sync/thread_sync.cpp
+++ thread_sync/thread_sync/thread_sync.cpp
@@ -2,10 +2,43 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <windows.h>
+
+int g_num = 0;
+
+void func_1() {
+  while (g_num < 20) {
+    ++g_num;
+    Sleep(100);
+
+    std::cout << "func_1 g_num:" << g_num << std::endl;
+  }
+}
+
+void func_2() {
+  while (g_num < 20) {
+    ++g_num;
+    Sleep(100);
+
+    std::cout << "func_2 g_num:" << g_num << std::endl;
+  }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  std::thread thread_1(func_1);
+  std::thread thread_2(func_2);
+
+  if (thread_1.joinable()) {
+    thread_1.join();
+  }
+  if (thread_2.joinable()) {
+    thread_2.join();
+  }
+
+  
+  return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
